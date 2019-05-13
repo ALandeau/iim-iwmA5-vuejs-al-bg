86bbd4bf8be07a00156b94d272f9d0324e293374
ALandeau <landeau.alex@gmail.com>
2019-05-13 16:22:53 +0200

    [ADD] Basic Axios file

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,16 @@
 <template>
   <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        iim-iwmA5-vuejs-al-bg
-      </h1>
-      <h2 class="subtitle">
-        My remarkable Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
-    </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import axios from '~/plugins/axios'
 
 export default {
-  components: {
-    Logo
+  data() {
+    return {
+
+    }
   }
 }
 </script>
