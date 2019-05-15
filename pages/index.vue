8c7ecf2b719066eb30e68c7377353e18d5e68387
ALandeau <landeau.alex@gmail.com>
2019-05-15 14:34:52 +0200

    [STYLE] Add Schedule style

--- pages/index.vue
+++ pages/index.vue
@@ -13,12 +13,35 @@
     <div class="column">
       <article v-if="posts && posts.length">
         <div class="card" v-for="post of posts" :key="post.id">
-          <header class="card-header">
+          <header class="card-header" v-on:click="modalSchedule(post.id)">
             <p class="card-header-title">
               {{ post.name }}
             </p>
+            <a href="#" class="card-header-icon" aria-label="more options">
+              <span class="icon">
+                <i :id="'icon-close-schedule'+post.id" class="fas fa-angle-down"></i>
+                <i :id="'icon-open-schedule'+post.id" class="icon-open-schedule fas fa-angle-up"></i>
+              </span>
+            </a>
           </header>
           <div class="card-content">
+            <div :id="'schedule-element' + post.id" class="schedule-box">
+              <div v-for="schedule in post.schedule" :key="schedule.id">
+                <div class="tabs is-small">
+                  <ul>
+                    <li v-if="day === currentDay" v-for="day in days" :key="day" class="is-active">
+                      <a v-on:click="updateDay(day)" class="day-button">{{ day }}</a>
+                    </li>
+                    <li v-else>
+                      <a v-on:click="updateDay(day)" class="day-button">{{ day }}</a>
+                    </li>
+                  </ul>
+                </div>
+                <schedule-component
+                  :select-day="currentDay"
+                  :schedule="schedule"/>
+              </div>
+            </div>
             <div class="content">
               {{post.departure[0].name}} -> {{post.arrived[0].name}}
             </div>
@@ -35,15 +58,20 @@
 
 <script>
 import axios from '~/plugins/axios'
+import ScheduleComponent from "../components/scheduleComponent";
 
 const apiUrl = "http://localhost:3001/"
 
 export default {
+  components: {ScheduleComponent},
   data() {
     return {
       posts: [],
       destination: [],
-      errors: []
+      errors: [],
+      days: ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"],
+      currentDay: "monday",
+      show: false
     }
   },
 
@@ -109,12 +137,32 @@ export default {
           response.data.departure = getDeparture(response.data, destination)
           response.data.arrived = getArrived(response.data, destination)
           this.posts = this.posts.map(function( obj ) {
-            return obj.id == id ? response.data : obj
+            if (obj.id === id) {
+              response.data.schedule = obj.schedule
+              return response.data
+            }
+            return obj
           });
         })
         .catch(e => {
           this.errors.push(e)
         })
+    },
+    modalSchedule: function(id) {
+      if (this.show === false) {
+        this.show = true;
+        document.getElementById('schedule-element'+id).style.display = "block";
+        document.getElementById('icon-close-schedule'+id).style.display = "none";
+        document.getElementById('icon-open-schedule'+id).style.display = "block"
+      } else {
+        this.show = false;
+        document.getElementById('schedule-element'+id).style.display = "none";
+        document.getElementById('icon-close-schedule'+id).style.display = "block";
+        document.getElementById('icon-open-schedule'+id).style.display = "none"
+      }
+    },
+    updateDay: function(day) {
+      this.currentDay = day;
     }
   }
 }
@@ -132,3 +180,26 @@ function getArrived(obj, destination) {
 }
 
 </script>
+
+<style lang="scss">
+  .card {
+    .card-header{
+      cursor: pointer;
+    }
+    .schedule-box {
+      display: none;
+    }
+    .icon-open-schedule {
+      display: none;
+    }
+    .day-button {
+      text-transform: capitalize;
+    }
+    .schedule-content {
+      margin: 10px 0 40px;
+      .tag {
+        margin: 0 2px;
+      }
+    }
+  }
+</style>
